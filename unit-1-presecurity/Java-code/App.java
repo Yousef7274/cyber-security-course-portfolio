a37d7febf44cf82766db916a4ed5d46676000213
Yousef <abdulhameedyousef13@gmail.com>
2026-09-04 14:02:07 +0300

    Add files via upload

---
+++ unit-1-presecurity/Java-code/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+String tekija = "Yousef";
+int luku1 = 7;
+int luku2 = 4;
+int luku3 = 0;
+
+System.out.println("Hei olen Tulostin-ohjelma");
+System.out.println("ohjelman tekijä: " + tekija);
+tulo = luku1 * luku2;
+System.out.println(luku1 + "*" + luku2 + "=" tulo);
+int summa = luku1 + luku2;
+System.out.println(luku1 + "+" + luku2 + "=" summa);
+int erotus = luku1 - luku2;
+System.out.println(luku1 + "-" + luku2 + "=" erotus);
+double jako = jakoluku1 / jakoluku2
+System.out.println(luku1 + "/" + luku2 + "=" jako);
